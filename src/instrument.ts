f52a0ad9c4763605fc382f26a7a02e2224b05869
조찬영 <cyjo9603@naver.com>
2025-01-27 19:02:34 +0900

    sentry profile module 제거

--- src/instrument.ts
+++ src/instrument.ts
@@ -1,10 +1,8 @@
 import * as Sentry from '@sentry/nestjs';
-import { nodeProfilingIntegration } from '@sentry/profiling-node';
 
 export function initSentry(dsn: string) {
   Sentry.init({
     dsn,
-    integrations: [nodeProfilingIntegration()],
     // Tracing
     tracesSampleRate: 1.0, //  Capture 100% of the transactions
   });
